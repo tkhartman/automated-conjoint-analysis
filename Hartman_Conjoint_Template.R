703ccb894e3f3ca631e74104f07e096dc7f136b4
Todd K. Hartman <tkhartman@gmail.com>
2017-04-21 09:20:12 +0100

    Update Hartman_Conjoint_Template.R

--- Hartman_Conjoint_Template.R
+++ Hartman_Conjoint_Template.R
@@ -18,16 +18,42 @@ getwd()  # Get the current working directory
 ## 1. ENTER your Qualtrics file name (exactly as it appears without the .csv; quotes required)
 qualtrics <- "ENTER YOUR FILE NAME HERE"
 
-## 2. ENTER the number of different conjoint experiments (default is 1)
+## 2. ENTER the number of different conjoint experiments (default = 1)
 cj.exp <- 1
 
 ## 3. LIST preferences from each conjoint experiment (answer choices)
-cj.prefs <- vector(mode="list", cj.exp)
+## If there is more than a single conjoint experiment, then
+## uncomment and run 'cj.prefs[[2]]', 'cj.prefs[[3]]', etc.
+## (each unique conjoint experiment should have its own 'cj.prefs[[i]]' object)
+cj.prefs <- vector(mode = "list", cj.exp)
 cj.prefs[[1]] <- c("ENTER", "ENTER", "ENTER")  # Conjoint 1, replace 'ENTER' with each variable name
 
-##' Prepare the data
+## 4. LIST attributes (main categories) from each conjoint experiment
+## These will serve as the predictors for our models
+cj.model <- vector(mode = "list", cj.exp)
+cj.model[[1]] <- selected ~ `ENTER ATTRIBUTE 1` + `ENTER ATTRIBUTE 2` + `ENTER ATTRIBUTE 3`
+
+## 5. [OPTIONAL] LIST variables for subgroup analysis for each conjoint experiment
+cj.segments[[1]] <- vector(mode = "list", cj.exp)
+cj.segments[[1]] <- c("ENTER", "ENTER", "ENTER")  # Uncomment and replace 'ENTER' with each variable name
+
+## 6. SET the baseline category for each attribute
+## Must match EXACTLY as how it is written in the .php file, including spaces and capitalization
+## (each conjoint experiment should have its own baselines object)
+baselines <- vector(mode = "list", cj.exp)  # Create an empty list to store the names
+baselines[[1]]$`ENTER ATTRIBUTE 1 HERE` <- "ENTER BASELINE VALUE 1 HERE"  # Uncomment and run
+baselines[[1]]$`ENTER ATTRIBUTE 1 HERE` <- "ENTER BASELINE VALUE 2 HERE"  # Uncomment and run
+baselines[[1]]$`ENTER ATTRIBUTE 1 HERE` <- "ENTER BASELINE VALUE 3 HERE"  # Uncomment and run
+baselines[[1]]$`ENTER ATTRIBUTE 2 HERE` <- "ENTER BASELINE VALUE 1 HERE"  # Uncomment and run
+baselines[[1]]$`ENTER ATTRIBUTE 2 HERE` <- "ENTER BASELINE VALUE 2 HERE"  # Uncomment and run
+baselines[[1]]$`ENTER ATTRIBUTE 2 HERE` <- "ENTER BASELINE VALUE 3 HERE"  # Uncomment and run
+
+
+
+
+##' Prepare the data for analysis
 data.name <- paste(qualtrics, "csv", sep = ".")  # Create the file name
-df.raw <- read.csv(data.name, header = FALSE, check.names = FALSE)  ## Load the raw .csv
+df.raw <- read.csv(data.name, header = FALSE, check.names = FALSE, stringsAsFactors = FALSE)  ## Load the raw .csv
 
 ## Fix the variable names and make sure first 2 rows match
 df.raw[2, ] <- df.raw[1, ]
@@ -35,60 +61,21 @@ df.raw <- df.raw[-3, ]
 colnames(df.raw) = as.character(unlist(df.raw[1, ]))
 df.raw = df.raw[-1, ]
 
-## Print the variable names
-names(df.raw)
-
 ## Create a new unique respondent identifier
 counter <- nrow(df.raw) - 1
 df.raw$cj.id <- seq(from = 0, to = counter, by = 1)
 df.raw$cj.id[1] <- "cj.id"
 
-## IF YOU HAVE MORE THAN 1 CONJOINT EXPERIMENT, THEN
-## uncomment and run 'cj.prefs[[2]]', 'cj.prefs[[3]]', etc.
-## (each conjoint experiment should have 'cj.prefs[[i]]' object)
-# cj.prefs[[2]] <- c("ENTER", "ENTER", "ENTER")  
-
-## 5. (Optional) ENTER variables for sub-analysis by groups (Segmentation)
-# cj.segments <- c("ENTER", "ENTER", "ENTER")  # Uncomment and replace 'ENTER' with each variable name
-
-## 6. SET the baseline category for each attribute
-## (Must match EXACTLY as how it is written in the .php file, 
-## including spaces and capitalization)
-for (j in 1:cj.exp){
-    ## Display attributes from each conjoint experiment
-    x <- paste("^", paste(rep("F", j), collapse = ""), "-1-1", "$", sep = "")
-    y <- grep(x, names(df.raw))
-    cat("Conjoint", j, "\n")
-    print(levels(df.raw[, y]))
-}
-
-baselines <- vector(mode="list", cj.exp)  # Create an empty list to store the names
-
-baselines[[1]]$`ENTER ATTRIBUTE HERE` <- "ENTER BASELINE VALUE HERE"  # Uncomment and run
-
-## IF YOU HAVE MORE THAN 1 CONJOINT EXPERIMENT, THEN
-## (each conjoint experiment should have its own baselines object)
-# baselines[[2]]$`ENTER ATTRIBUTE HERE` <- "ENTER BASELINE VALUE HERE"  # Uncomment and run
-
-## 7. ENTER the conjoint attributes from the .php file (see #6 above)
-## We'll use this to build the model for the analysis
-cj.model <- vector(mode="list", cj.exp)
-cj.model[[1]] <- selected ~ `ENTER` + `ENTER` + `ENTER`
-
-## IF YOU HAVE MORE THAN 1 CONJOINT EXPERIMENT, THEN
-## (each conjoint experiment should have its own cj.model object)
-# cj.model[[2]] <- selected ~ `ENTER` + `ENTER` + `ENTER`  # Uncomment and run
-
-##' Run the conjoint analysis
-cj.attr <- cj.keep <- df.sub <- data.name.sub <- df <- results <- vector(mode="list", cj.exp)
+## Create the datasets, save to unique .csv files
+cj.attr <- cj.keep <- df.sub <- data.name.sub <- df <- results <- vector(mode = "list", cj.exp)
 
 for (i in 1:cj.exp) {
     ## Identify embedded variables (attributes respondents saw; default is 'F')
     z <- paste(paste("^", paste(rep("F", i), collapse = ""), sep = ""), "\\-", sep = "")
     cj.attr[[i]] <- grep(z, names(df.raw), value = TRUE)
     ## Variables to keep
-    if (exists("cj.segments") == TRUE) {
-        cj.keep[[i]] <- c("cj.id", cj.attr[[i]], cj.prefs[[i]], cj.segments)  
+    if (exists(cj.segments[[i]]) == TRUE) {
+        cj.keep[[i]] <- c("cj.id", cj.attr[[i]], cj.prefs[[i]], cj.segments[[i]])  
     } else {
         cj.keep[[i]] <- c("cj.id", cj.attr[[i]], cj.prefs[[i]])          
     }
@@ -96,29 +83,34 @@ for (i in 1:cj.exp) {
     df.sub[[i]] <- df.raw[cj.keep[[i]]]
     ## Remove missing values
     df.sub[[i]] <- na.omit(df.sub[[i]])
-    ## Create a file name
-    data.name.sub[[i]] <- paste(paste(qualtrics, i, sep = "_"), ".csv", sep = "")  
+    ## Create the file name
+    data.name.sub[[i]] <- paste(qualtrics, i, sep = "_") 
+    file.name.sub[[i]] <- paste(data.name.sub[[i]], ".csv", sep = "")  
     ## Save the subsetted data so that it can be loaded
     write.csv(df.sub[[i]],   
-              data.name.sub[[i]], 
+              file.name.sub[[i]], 
               row.names = FALSE) 
     ## Load the subsetted data into a conjoint data frame
-    if (exists("cj.segments") == TRUE) {
-        df[[i]] <- read.qualtrics(data.name.sub[[i]],         # Subsetted data
+    if (exists(cj.segments[[i]]) == TRUE) {
+        df[[i]] <- read.qualtrics(file.name.sub[[i]],         # Subsetted data
                                   respondentID = "cj.id",     # Unique respondent ID
                                   responses = cj.prefs[[i]],  # Selected option
-                                  covariates = "cj.segments") # Grouping variables 
+                                  covariates = cj.segments[[i]]) # Grouping variables 
     }
     else {
-        df[[i]] <- read.qualtrics(data.name.sub[[i]],         # Subsetted data
+        df[[i]] <- read.qualtrics(file.name.sub[[i]],         # Subsetted data
                                   respondentID = "cj.id",     # Unique respondent ID
                                   responses = cj.prefs[[i]])  # Selected option
     } 
     cat("\n", "Processing Conjoint", i, "\n")
-    ## Save the converted data for use with other statistical software
+    ## Save the converted long format data
     write.csv(df[[i]], 
               paste(data.name.sub[[i]], "conjoint.csv", sep = "_"), 
               row.names = FALSE)
+}
+
+##' Run the main conjoint analysis and save plots to .pdf files
+for (i in 1:cj.exp) {
     ## Conjoint analysis - calculate AMCE estimator using all attributes in the design
     results[[i]] <- amce(data = df[[i]],                   # The conjoint dataset
                        cj.model[[i]],                         # The model
@@ -130,7 +122,9 @@ for (i in 1:cj.exp) {
     print(summary(results[[i]]))
 
     ## Create figures
+    pdf(paste("figure, i, ".pdf", sep = ""), width = 21/2.54, height = 29.7/2.54)
     plot(results[[i]], 
          main = substitute(paste("Conjoint", i)),
          xlab = "Change in Predicted Probability")
+    dev.off()
 }
