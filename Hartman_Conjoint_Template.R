509571bf5e62b37ca0fc9d5484437d64df214bb7
Todd K. Hartman <tkhartman@gmail.com>
2017-09-01 10:34:51 +0100

    Update Hartman_Conjoint_Template.R

--- Hartman_Conjoint_Template.R
+++ Hartman_Conjoint_Template.R
@@ -122,7 +122,7 @@ for (i in 1:cj.exp) {
     ## Create figures
     pdf(paste("figure, i, ".pdf", sep = ""), width = 21/2.54, height = 29.7/2.54)
     plot(results[[i]], 
-         main = substitute(paste("Conjoint", i)),
+         main = paste("Conjoint", i, sep = " ")),
          xlab = "Change in Predicted Probability")
     dev.off()
 }
